c6e61d8444885c7b7432e8ebf0446b68b0d99d7d
Justin <s101407028@gmail.com>
2022-10-23 09:41:09 +0800

    seperate the route: /shortenURL from index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const URL = require('../models/URL')
 const root = require('./modules/root')
+const shortenURL = require('./modules/shortenURL')
 
 const router = express.Router()
 
-//root directory
+//root directory (home page)
 router.use('/', root)
+
+//shortenURL
+router.use('/shortenURL', shortenURL)
 
 //redirect
 router.get("/:inputShortURL", (req, res) => {
--- routes/modules/root.js
+++ routes/modules/root.js
@@ -1,47 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const URL = require('../../models/URL')
-const shortURL = require('../../utils/generate_shortURL')
 
 //homepage
 router.get('/', (req, res) => {
   res.render('index')
 })
 
-//CREATE
-router.post('/', (req, res) => {
-
-  const inputURL = req.body.initURL
-
-  //input驗證: 當輸入input的網址不含https等protocal則顯示提醒字樣(包含使用者未輸入任何字就按下按钮的情況)
-  const obj = require('url').parse(inputURL)
-  const urlProtocol = obj.protocol
-  if (!urlProtocol) {
-    res.render('index', { obj, inputURL })
-    return
-  }
-
-  URL.findOne({ initURL: inputURL })
-    .lean()
-    .then((data) => {
-
-      //if the input URL has been shortened before
-      if (data) {
-
-        res.render('index', { data, inputURL, origin: req.headers.origin, shortURL: data.shortURL })
-
-        //if never been shortened before
-      } else {
-        const randomShortURL = shortURL(5)
-        //未來優化方向
-        //需檢查新產生的shortURL是否與database中的重複
-        URL.create({ initURL: inputURL, shortURL: randomShortURL })
-          .then(
-            res.render('index', { inputURL, origin: req.headers.origin, randomShortURL })
-          )
-      }
-    })
-    .catch(error => console.log(error))
-})
 
 module.exports = router
